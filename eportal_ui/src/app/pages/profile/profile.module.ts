fbcfb31d6c2595a4d08e30729913c9de49b76e05
Zng Tfy <nvt87x@gmail.com>
2018-09-25 08:51:46 +0700

    Add back

---
+++ eportal_ui/src/app/pages/profile/profile.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ProfileComponent } from './profile.component';
+
+export const routes = [
+  { path: '', component: ProfileComponent, pathMatch: 'full' }
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    RouterModule.forChild(routes)
+  ],
+  declarations: [ProfileComponent]
+})
+
+export class ProfileModule { }
